e709f905bd822a18b898f5ad387fb8fddd942a3c
Kateryna Gapchich <gapchich.kate@gmail.com>
2019-05-17 13:13:59 +0200

    1.py

--- 1.py
+++ 1.py
@@ -1,33 +1,25 @@
 import urllib.request
 import re
-
+import os
 import requests
 
 
 def get_path_to_file():
     return "/home/pyatka_jeka/comicses-links"
 
-
-# TODO: func with the request to page source, remake download to different folders and for a several comicses
-
+#TODO: make it asyncio
 
 def main():
     with open(get_path_to_file(), "r") as f:
         links_new = f.readlines()
-        makeDictionary(links_new)
-    '''
-    i = 0
-    url = links[0][0:len(links[0]) - 1]
-    end_page_prepare = links[1].split("/")
-    end_page = end_page_prepare[-1]
-    for i in range(int(end_page)):
-        i += 1
-        page_request = requests.get(url + str(i))
-        page_source_content = page_request.text
-        arr_with_content = page_source_content
-        image_link = find_link_for_image(arr_with_content)
-        download_image(image_link)
-        '''
+    dict_with_links = makeDictionary(links_new)
+    for x in dict_with_links:
+        curr_comics_name = make_comics_name(x)
+        create_folder(curr_comics_name)
+        for i in range(int(dict_with_links[x])):
+            page_source_content = page_source_request(x, i)
+            image_link = find_link_for_image(page_source_content)
+            download_image(image_link, curr_comics_name)
 
 
 def find_link_for_image(page_content):
@@ -36,8 +28,10 @@ def find_link_for_image(page_content):
     return image_link
 
 
-def page_source_request(string_with_link):
-    pass
+def page_source_request(string_with_link, counter):
+    page_request = requests.get(string_with_link + str(counter))
+    page_source_content = page_request.text
+    return page_source_content
 
 
 def makeDictionary(list_with_links):
@@ -52,8 +46,26 @@ def makeDictionary(list_with_links):
     return dict_with_comicses_links
 
 
-def download_image(image_link):
-    urllib.request.urlretrieve(image_link, "/home/pyatka_jeka/comicses_images/" + image_link.split('/')[-1])
+def make_comics_name(link_string):
+    comics_name_start_index = link_string.find('~') + 1
+    comics_name_end_index = link_string[comics_name_start_index:].find('/')
+    comics_name = link_string[comics_name_start_index:][:comics_name_end_index]
+    return comics_name
+
+
+def download_image(image_link, comics_name):
+    urllib.request.urlretrieve(image_link, "/home/pyatka_jeka/" + comics_name + "/" + image_link.split('/')[-1])
+
+
+def create_folder(comics_name):
+    path = "/home/pyatka_jeka/" + comics_name
+    if os.path.exists(path):
+        pass
+    else:
+        try:
+            os.mkdir(path)
+        except OSError:
+            print("Creation of the directory %s failed" % path)
 
 
 if __name__ == "__main__":
