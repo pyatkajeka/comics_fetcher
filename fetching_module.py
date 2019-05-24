8747aab75b6758c0fa7628cf5d18ea7d0167d10a
Kateryna Gapchich <gapchich.kate@gmail.com>
2019-05-24 12:18:22 +0200

    main.py

--- fetching_module.py
+++ fetching_module.py
@@ -2,29 +2,6 @@ import urllib.request
 import re
 import os
 import requests
-import sys
-
-def get_path_to_file():
-    if (len(sys.argv) == 1):
-        return "/home/pyatka_jeka/comicses-links"
-    else:
-        return str(sys.argv[1])
-
-
-#TODO: make it asyncio
-#TODO: make requirements.txt
-
-def main():
-    with open(get_path_to_file(), "r") as f:
-        links_new = f.readlines()
-    dict_with_links = make_dictionary(links_new)
-    for link, end_page in dict_with_links.items():
-        curr_comics_name = make_comics_name(link)
-        create_folder(curr_comics_name)
-        for i in range(int(end_page)):
-            page_source_content = page_source_request(link, i)
-            image_link = find_link_for_image(page_source_content)
-            download_image(image_link, curr_comics_name)
 
 
 def find_link_for_image(page_content):
@@ -68,7 +45,3 @@ def create_folder(comics_name):
             os.mkdir(path)
         except OSError:
             print(f"Creation of the directory {path} failed")
-
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+import fetching_module
+
+
+def get_path_to_file():
+    if (len(sys.argv) == 1):
+        return "/home/pyatka_jeka/comicses-links"
+    else:
+        return str(sys.argv[1])
+
+
+#TODO: make it asyncio
+
+def main():
+    with open(get_path_to_file(), "r") as f:
+        links_new = f.readlines()
+    dict_with_links = fetching_module.make_dictionary(links_new)
+    for link, end_page in dict_with_links.items():
+        curr_comics_name = fetching_module.make_comics_name(link)
+        fetching_module.create_folder(curr_comics_name)
+        for i in range(int(end_page)):
+            page_source_content = fetching_module.page_source_request(link, i)
+            image_link = fetching_module.find_link_for_image(page_source_content)
+            fetching_module.download_image(image_link, curr_comics_name)
+
+
+if __name__ == "__main__":
+    main()
